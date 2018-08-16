99f7853530adbe872ec618d1be5c3cceed1aade5
Jamel Toms <jameltoms@gmail.com>
2018-08-16 01:09:52 -0400

    Moved random text generation to single function. Print number of movies. Use more defaults in index generation.

--- test/run.test.js
+++ test/run.test.js
@@ -1,23 +1,19 @@
 const randomize = require('randomatic');
-const { stemmer } = require('porter-stemmer');
 const { times, sample, random } = require('lodash');
 const { before } = require('mocha');
 const { movies } = require('./movie-data');
-const {
-  Search,
-  StemmingTokenizer,
-  SimpleTokenizer,
-  TfIdfSearchIndex,
-  AllSubstringsIndexStrategy
-} = require('js-search');
+const { Search, AllSubstringsIndexStrategy } = require('js-search');
 
 const sizes = [1, 10, 100, 1000, 2000, 5000, 10000];
 const fields = ['id', 'name', 'details', 'location'];
 
+function text() {
+  return randomize('a', 25);
+}
+
 function doc() {
   return fields.reduce((acc, cur) => {
-    acc[cur] =
-      randomize('a', 10) + ' ' + randomize('a', 12) + ' ' + randomize('a', 8);
+    acc[cur] = text();
     return acc;
   }, {});
 }
@@ -28,8 +24,6 @@ function docs(length) {
 
 function build() {
   const search = new Search(fields[0]);
-  search.tokenizer = new StemmingTokenizer(stemmer, new SimpleTokenizer());
-  search.searchIndex = new TfIdfSearchIndex(fields[0]);
   search.indexStrategy = new AllSubstringsIndexStrategy();
   fields.forEach(field => search.addIndex(field));
   return search;
@@ -42,8 +36,6 @@ describe('index', () => {
   describe('movies', () => {
     before(() => {
       search = new Search('title');
-      search.tokenizer = new StemmingTokenizer(stemmer, new SimpleTokenizer());
-      search.searchIndex = new TfIdfSearchIndex('title');
       search.indexStrategy = new AllSubstringsIndexStrategy();
       search.addIndex('title');
       search.addIndex('year');
@@ -54,13 +46,14 @@ describe('index', () => {
     });
 
     it('build', () => {
+      console.log(`Adding ${movies.length} documents`);
       search.addDocuments(movies);
     });
 
     it('search', () => {
       const query = 'caught';
       console.log(`searching for: ${query}`);
-      let results = search.search(query);
+      const results = search.search(query);
       console.log(JSON.stringify(results, null, '  '));
     });
   });
@@ -77,13 +70,13 @@ describe('index', () => {
       });
 
       it('search', () => {
-        let ref = sample(documents);
-        let field = sample(fields);
-        let begin = random(0, 10);
-        let end = random(10, 25);
-        let substr = ref[field].substring(begin, end);
-        console.log(`searching for: ${substr}`);
-        let results = search.search(substr);
+        const ref = sample(documents);
+        const field = sample(fields);
+        const begin = random(0, 10);
+        const end = random(10, 25);
+        const text = ref[field].substring(begin, end);
+        console.log(`searching for: ${text}`);
+        const results = search.search(text);
         console.log(JSON.stringify(results, null, '  '));
       });
     });
